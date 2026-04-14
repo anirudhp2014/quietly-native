78ec2eadb9294257c85d72e6f103f5fc8fb17e75
anirudh singh parmar <anirudhsinghparmar@anirudhs-MacBook-Air.local>
2026-04-14 07:33:48 +0530

    fix: add metro path alias resolution for @/* -> src/*

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,20 @@
+const path = require('path');
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
+
+// Resolve @/* path alias to src/*
+config.resolver.extraNodeModules = new Proxy(
+  {},
+  {
+    get: (_target, name) => {
+      if (name === '@') {
+        return path.resolve(__dirname, 'src');
+      }
+      return path.resolve(__dirname, 'node_modules', String(name));
+    },
+  }
+);
 
 module.exports = withNativeWind(config, { input: './src/global.css' });
